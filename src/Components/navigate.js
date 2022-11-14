2e31b6e52fd310b6859ce6d8e329570ae95fe60f
TonyH98 <tonyhoang@pursuit.org>
2022-11-14 13:26:05 -0500

    Nav bar finished

--- src/Components/navigate.js
+++ src/Components/navigate.js
@@ -1,4 +1,19 @@
+import {Link} from "react-router-dom"
 
-const Navigate = () => {
-    return
+
+export default Navigate = () => {
+    return(
+        <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    )
 }
