a1cce6fb7d4c045bb49c94c6198f8db1c98d8afd
TonyH98 <tonyhoang@pursuit.org>
2022-11-14 13:22:18 -0500

    About page almost done

---
+++ src/Components/home.js
@@ -0,0 +1,14 @@
+
+export default function Home () {
+    return (
+        <div>
+            <label htmlFor="search">Search
+            <input
+            type="text"
+            placeholder="Search">
+            </input>
+            </label>
+            <button>Submit</button>
+        </div>
+    )
+}
---
+++ src/Components/navigate.js
@@ -0,0 +1,4 @@
+
+const Navigate = () => {
+    return
+}
