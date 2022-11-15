760ca05894c5aeb59d2fd42ae0f4188cfc94005e
TonyH98 <tonyhoang@pursuit.org>
2022-11-15 10:27:26 -0500

    Small css changes

--- src/Components/home.js
+++ src/Components/home.js
@@ -4,12 +4,9 @@ import "./home.css"
 export default function Home () {
     return (
         <div className="search-feature">
-            <label htmlFor="search">
-            Search:
-            </label>
             <input
             type="text"
-            placeholder="Search">
+            placeholder="Search...">
             </input>
             <button>Submit</button>
         </div>
