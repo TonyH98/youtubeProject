a620eb9a41823fae99f8370945183d0b90b90c40
Tony Hoang <107489887+TonyH98@users.noreply.github.com>
2022-11-16 21:01:20 -0500

    Revert "search bar 95% functioning, listing results if there a search term"

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,69 +1,40 @@
-import { useState, useEffect} from "react"
-import Snowboard from "../data"
+ import { useState} from "react"
+ import Snowboard from "../data"
 import ResultError from "../Components/errors/ResultsError"
-import Video from "./Video"
-import {getYoutubeVideos} from '../api/fetch'
+ import Video from "./Video"
 
-import "./home.css"
+ import "./home.css"
 
-export default function Home () {
-const [youtube , setYoutube] = useState([])
+ export default function Home () {
+ const [youtube , setYoutube] = useState(Snowboard)
 //  const [resultError, setResultError] = useState(false)
-const [search, setSearch] = useState("");
 
 
-useEffect(() => {
-   getYoutubeVideos(search)
-     .then((response) => {
-         setYoutube(response.items);
-
-       })
-       .catch((error) => {
-
-       });
-   }, [search]);
-
-   
-
-   function handleTextChange(event) {
-    const title = event.target.value;
-  
-  
-    setSearch(title);
-
-  
-  }
-//   console.log(youtube)
-//   console.log(search)
-    return (
-        <div className="search-feature">
-           <input
-            className="search"
-            type="text"
-            placeholder="Search..."
-            onChange={handleTextChange}/>
-            
-            <button>Submit</button>
-        <div>
-            <br></br>
-            <br></br>
-
-            {!search ? (null):(
-            <div className="thumbnails">
-                
-       
-        {
-           youtube.map((yt) => {
-               return (
-                   <Video key={yt.id.videoId} yt={yt}/>
-               )
-           })
-        }
-            </div>
-            )}
-        </div>
-        </div>
-    )
-}
+    
+     return (
+         <div className="search-feature">
+            <input
+             className="search"
+             type="text"
+             placeholder="Search...">
+             </input>
+             <button>Submit</button>
+         <div>
+             <br></br>
+             <br></br>
+             <div className="thumbnails">
+        
+         {
+            youtube.map((yt) => {
+                return (
+                    <Video key={yt.id.videoId} yt={yt}/>
+                )
+            })
+         }
+             </div>
+         </div>
+         </div>
+     )
+ }
 
 
