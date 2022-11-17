48f9085bb044d64d35000e26c0d61dd7155e6ec0
chrisking718 <chrisking@pursuit.org>
2022-11-16 20:51:36 -0500

    search bar 95% functioning, listing results if there a search term

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,40 +1,69 @@
- import { useState} from "react"
- import Snowboard from "../data"
+import { useState, useEffect} from "react"
+import Snowboard from "../data"
 import ResultError from "../Components/errors/ResultsError"
- import Video from "./Video"
+import Video from "./Video"
+import {getYoutubeVideos} from '../api/fetch'
 
- import "./home.css"
+import "./home.css"
 
- export default function Home () {
- const [youtube , setYoutube] = useState(Snowboard)
+export default function Home () {
+const [youtube , setYoutube] = useState([])
 //  const [resultError, setResultError] = useState(false)
+const [search, setSearch] = useState("");
 
 
-    
-     return (
-         <div className="search-feature">
-            <input
-             className="search"
-             type="text"
-             placeholder="Search...">
-             </input>
-             <button>Submit</button>
-         <div>
-             <br></br>
-             <br></br>
-             <div className="thumbnails">
-        
-         {
-            youtube.map((yt) => {
-                return (
-                    <Video key={yt.id.videoId} yt={yt}/>
-                )
-            })
-         }
-             </div>
-         </div>
-         </div>
-     )
- }
+useEffect(() => {
+   getYoutubeVideos(search)
+     .then((response) => {
+         setYoutube(response.items);
+
+       })
+       .catch((error) => {
+
+       });
+   }, [search]);
+
+   
+
+   function handleTextChange(event) {
+    const title = event.target.value;
+  
+  
+    setSearch(title);
+
+  
+  }
+//   console.log(youtube)
+//   console.log(search)
+    return (
+        <div className="search-feature">
+           <input
+            className="search"
+            type="text"
+            placeholder="Search..."
+            onChange={handleTextChange}/>
+            
+            <button>Submit</button>
+        <div>
+            <br></br>
+            <br></br>
+
+            {!search ? (null):(
+            <div className="thumbnails">
+                
+       
+        {
+           youtube.map((yt) => {
+               return (
+                   <Video key={yt.id.videoId} yt={yt}/>
+               )
+           })
+        }
+            </div>
+            )}
+        </div>
+        </div>
+    )
+}
 
 
